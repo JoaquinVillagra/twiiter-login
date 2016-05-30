73b9e6f6a33aa54b6ec985db4d1d5b4cbac4812d
Joaquin Villagra <Joaco@MacBook-Air-de-Joaquin.local>
2016-05-30 12:39:52 -0300

    API separada en config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+var config = {
+  twitter: {
+    key: TWITTER_API_KEY,
+    secret: TWITTER_API_SECRET
+  },
+  //facebook: {
+   // id: FACEBOOK_APP_ID,
+    //secret: FACEBOOK_APP_SECRET
+  //}
+};
+
+module.exports = config;
